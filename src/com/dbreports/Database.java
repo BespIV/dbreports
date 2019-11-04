a33d2a5b0cd01acead06149df64afcf664d80189
BespIV <snake07.07@mail.ru>
2019-11-04 17:12:44 +0300

    исправлен баз с именем БД

--- src/com/dbreports/Database.java
+++ src/com/dbreports/Database.java
@@ -16,17 +16,15 @@ public class Database {
     Database(String pathDB, String nameDB){
         connectionString = "jdbc:sqlite:"+pathDB+nameDB;
         pathDb = pathDB;
-        nameDb = pathDB;
+        nameDb = nameDB;
     }
 
     public void connect(){
         try {
             conn = DriverManager.getConnection(connectionString);
             stat = conn.createStatement();
-            if (conn != null){
+            if (conn != null) {
                 Debugger.out("Database", "Connect to " + connectionString + " SUCCESS!");
-            } else{
-                Debugger.out("Database","Connect to " + connectionString + " SUCCESS!");
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -44,8 +42,7 @@ public class Database {
 
     public void initNewDb(){
         try {
-            stat.execute("CREATE TABLE IF NOT EXISTS 'cases' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'product' text,'signOON' boolean,'customer' text,'dateInserting' date);");
-            Debugger.out("Database","Таблица создана");
+            stat.execute("CREATE TABLE IF NOT EXISTS 'cases' ('id' INTEGER PRIMARY KEY AUTOINCREMENT, 'product' text, 'customer' text, 'dateInserting' date, 'signOON' boolean);");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -53,23 +50,41 @@ public class Database {
 
     public void addCase(Case oneCase){
         try {
-            PreparedStatement statement = conn.prepareStatement("INSERT INTO cases('product', 'signOON', 'customer', 'dateInserting') VALUES(?,?,?,?)");
+            PreparedStatement statement = conn.prepareStatement("INSERT INTO cases('product', 'customer', 'dateInserting', 'signOON') VALUES(?,?,?,?)");
             statement.setObject(1, oneCase.product);
-            statement.setObject(2, oneCase.signOON);
-            statement.setObject(3, oneCase.customer);
-            statement.setObject(4, oneCase.dateInserting);
+            statement.setObject(2, oneCase.customer);
+            statement.setObject(3, oneCase.dateInserting);
+            statement.setObject(4, oneCase.signOON);
             statement.execute();
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void executeSQL(String query) throws SQLException {
-        stat.execute(query);
+    public void executeSQL(String query) {
+        try {
+            stat.execute(query);
+            Debugger.out("Database", "Выполнен запрос: " + query);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
+    public void selectAll(){
+        try {
+            resSet = stat.executeQuery("SELECT * FROM cases");
+            while (resSet.next()){
+                Debugger.out("SQL",
+                        resSet.getString(1)+ " | " +
+                        resSet.getString(2)+ " | " +
+                        resSet.getString(3) + " | " +
+                        resSet.getString(4) + " | " +
+                        resSet.getString(5)
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
-
-    public void dbToString(){
     }
 }
