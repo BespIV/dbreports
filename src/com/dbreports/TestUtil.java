1fb891f6c36d2719e82cc52f2b51a72b7a8d9841
BespIV <snake07.07@mail.ru>
2019-11-04 15:05:01 +0300

    Добавлен main класс для проверки работы классов

---
+++ src/com/dbreports/TestUtil.java
@@ -0,0 +1,13 @@
+package com.dbreports;
+import com.dbreports.utils.RandomUtil;
+
+public class TestUtil {
+    public static void main(String[] args) {
+        RandomUtil randomString = new RandomUtil();
+        for (int i=0; i<3; i++){
+            Debugger.out(randomString.getRandomString(6));
+            Debugger.out(randomString.getRandomInteger(6));
+            Debugger.out(randomString.getRandomDate(2019));
+        }
+    }
+}
