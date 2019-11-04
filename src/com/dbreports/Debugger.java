e6491bd315a690e4d4641f3d8adefa16a523bca1
BespIV <snake07.07@mail.ru>
2019-11-04 15:05:54 +0300

    + метод с перегрузкой по типу

--- src/com/dbreports/Debugger.java
+++ src/com/dbreports/Debugger.java
@@ -1,7 +1,11 @@
 package com.dbreports;
 
 class Debugger {
-    static void out(String inString){
+    public static void out(String inString){
         System.out.println("[DEBUG]: " + inString);
+    }
+
+    public static void out(int inInteger) {
+        System.out.println("[DEBUG]: " + String.valueOf(inInteger));
     }
 }
